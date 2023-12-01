57e7863ad03078b670e385823e4ee34205f8aa9d
ericdilukeyrus <125888946+ericdilukeyrus@users.noreply.github.com>
2023-12-01 13:32:19 +0000

    0.1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,52 +1,33 @@
 import streamlit as st
+import pandas as pd
 
-# Initialize connection.
-conn = st.connection("snowflake")
+def hr_data_frame():
+    # Initialize connection.
+    conn = st.connection("snowflake")
+    session = conn.session()
 
-# Perform query.
-#df = conn.query("SELECT * from LINKEDINLICENSES;", ttl=600)
+    #@st.cache_data()
+    def get_hr_data():
+        df = session.table("LINKEDINLICENSES").to_pandas() #get all data from the table
+        return  df
 
-# Load the table as a dataframe usng the Snowpark session.
-#@st.cache_data
-session  =conn.session()
-df = session.table("LINKEDINLICENSES")
-
-
-# Display data in an editable table
-
-edited_data = st.data_editor(
-    df,    
-    num_rows="dynamic",
-    column_config={
-        "TITLE": st.column_config.Column("Title"),
-        "RECRUITER_SEATLICENCE_NUMBER": st.column_config.Column("Recruiter Seatlicence #", width="small"),
-        "RECRUITER_SEAT_LICENCE_PRICE": st.column_config.Column("Recruiter Seat licence price"),
-        "DISCOUNT_RECRUITER_SEAT_PERCENT": st.column_config.Column("Discount Recruiter Seat %"),
-        "JOB_SLOTS_NUMBER": st.column_config.Column("Job slots #"),
-        "JOB_SLOTS_TOTAL_PRICE": st.column_config.Column("Job slots total price"),
-        "JOB_SLOT_DISCOUNT_PERCENT": st.column_config.Column("Job Slot Discount %"),
-        "CAREER_PAGE": st.column_config.Column("Career page"),
-        "CAREER_PAGE_DISCOUNT_PERCENT": st.column_config.Column("Career Page discount %"),
-        "START_DATE": st.column_config.Column("	Start date"),
-        "END_DATE": st.column_config.Column("End date"),
-        "CONTRACT_END_DATE": st.column_config.Column("Contract End Date"),
-        "OWNER": st.column_config.Column("Owner"),
-        "CONTRACT": st.column_config.Column("Contract"),
-        "VALID_END_DATE": st.column_config.Column("Valid End Date"),
-        "VALID_CONTRACT_END_DATE": st.column_config.Column("Valid Contract End Date"),
-        "IS_VALID": st.column_config.Column("Is Valid"),
-        "TEST": st.column_config.Column("Test")
-    },
-    hide_index=True
-)
-
-
-table_name = 'LINKEDINLICENSES'
-# Button to submit changes
-if st.button('Submit Changes'):
-    #session.sql("TRUNCATE TABLE LINKEDINLICENSES").collect() #Truncate table
-    conn.write_pandas(edited_data,table_name, auto_create_table=True, overwrite=True)
+    df = get_hr_data() 
+     
+    edited_data = st.data_editor(
+        df,
+        column_config={
+            "CONTRACT": st.column_config.SelectboxColumn("Contrat",options=["LinkedIn-1 Years","LinkedIn-2 Years","LinkedIn-3 Years"])
+        },
+        width=1200,
+        use_container_width=True,
+        num_rows="dynamic")
     
-    
+    table_name = 'LINKEDINLICENSES'
+     # Button to submit changes
+    if st.button('Submit Changes'):
+        session.sql("TRUNCATE TABLE LINKEDINLICENSES").collect() #Truncate table
+        conn.write_pandas(edited_data,table_name, overwrite=False)
+
+hr_data_frame()
 
 
