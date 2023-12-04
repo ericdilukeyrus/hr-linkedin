888ce604d3249e35b5c11a7345c569f1a95d01d2
ericdilukeyrus <125888946+ericdilukeyrus@users.noreply.github.com>
2023-12-04 11:58:20 +0000

    sc

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,10 @@ import streamlit as st
 import pandas as pd
 
 def hr_data_frame():
-    st.write("Hello")
+    # Initialize connection.
+    conn = st.connection("snowflake")
+    session = conn.session()
+    
 hr_data_frame()
 
 
