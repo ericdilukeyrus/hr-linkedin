5b00746a8d169e2bc1ff0dbeecee609fc30a16c8
ericdilukeyrus <125888946+ericdilukeyrus@users.noreply.github.com>
2023-12-04 12:35:08 +0000

    form

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,30 @@ from snowflake.snowpark import Session
 def hr_data_frame():
 
     #Initialize connection.
-    #if 'sb'
-    with open('creds.json') as f:
-        connection_param = json.load(f)
-    ses = Session.builder.configs(connection_param).create()   
-    st.write(ses.table("LINKEDINLICENSES_"))
+    if 'snowflake_connection' not in st.session_state:
+        #Connect to Snowflake
+        with open('creds.json') as f:
+            connection_param = json.load(f)
+        st.session_state.snowflake_connection = Session.builder.configs(connection_param).create()   
+        session = st.session_state.snowflake_connection
+    else: 
+        session = st.session_state.snowflake_connection
+    
+    def get_dataset():
+        #Load table to df
+        df = session.table("LINKEDINLICENSES_")
+        return df
+
+    dataset = get_dataset()
+    #st.write(dataset)
+
+    with st.form("data_editor_form"):
+        st.caption("Edit the datafrae below")
+        edited_data = st.data_editor(
+            dataset,
+            use_container_width=True,
+            num_rows="dynamic")
+        submit_button = st.form_submit_button("Submit")
 
 hr_data_frame()
 
