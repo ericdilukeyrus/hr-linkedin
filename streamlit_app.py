fb25fb4504dc29d69f4d5753b90a02dab28d770c
ericdilukeyrus <125888946+ericdilukeyrus@users.noreply.github.com>
2023-12-04 12:18:40 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,17 @@
 import streamlit as st
 import pandas as pd
+import json
+from snowflake.snowpark import Session
 
 def hr_data_frame():
-    # Initialize connection.
-    conn = st.connection("snowflake")
-    session = conn.session()
-    
+
+    #Initialize connection.
+    if 's'
+    with open('creds.json') as f:
+        connection_param = json.load(f)
+    ses = Session.builder.configs(connection_param).create()   
+    st.write(ses.table("LINKEDINLICENSES_"))
+
 hr_data_frame()
 
 
